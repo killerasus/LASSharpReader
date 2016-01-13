2228bacd7abf0c891a6b81ca8619b2f9f11f3517
Bruno <killerasus@gmail.com>
2016-01-13 02:17:53 -0200

    Inserted loadLASButton, loadedFileLabel and loadedFileLabelTooltip components.

--- LASSharpReader/MainWindow.Designer.cs
+++ LASSharpReader/MainWindow.Designer.cs
@@ -28,20 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.openLASDialog = new System.Windows.Forms.OpenFileDialog();
+            this.loadLASButton = new System.Windows.Forms.Button();
+            this.fileLabel = new System.Windows.Forms.Label();
+            this.loadedFileLabelTooltip = new System.Windows.Forms.ToolTip(this.components);
             this.SuspendLayout();
             // 
-            // Form1
+            // openLASDialog
+            // 
+            this.openLASDialog.FileName = "openFileDialog1";
+            this.openLASDialog.Filter = "LAS files|*.las|Text files|*.txt";
+            this.openLASDialog.Title = "Load LAS file";
+            // 
+            // loadLASButton
+            // 
+            this.loadLASButton.Location = new System.Drawing.Point(13, 13);
+            this.loadLASButton.Name = "loadLASButton";
+            this.loadLASButton.Size = new System.Drawing.Size(75, 23);
+            this.loadLASButton.TabIndex = 0;
+            this.loadLASButton.Text = "Load LAS";
+            this.loadLASButton.UseVisualStyleBackColor = true;
+            this.loadLASButton.Click += new System.EventHandler(this.loadLASButton_Click);
+            // 
+            // fileLabel
+            // 
+            this.fileLabel.AutoSize = true;
+            this.fileLabel.Location = new System.Drawing.Point(94, 18);
+            this.fileLabel.Name = "fileLabel";
+            this.fileLabel.Size = new System.Drawing.Size(62, 13);
+            this.fileLabel.TabIndex = 1;
+            this.fileLabel.Text = "Loaded file:";
+            // 
+            // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "Form1";
+            this.Controls.Add(this.fileLabel);
+            this.Controls.Add(this.loadLASButton);
+            this.Name = "MainWindow";
             this.Text = "LASSharpReader";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openLASDialog;
+        private System.Windows.Forms.Button loadLASButton;
+        private System.Windows.Forms.Label fileLabel;
+        private System.Windows.Forms.ToolTip loadedFileLabelTooltip;
     }
 }
 
--- LASSharpReader/MainWindow.cs
+++ LASSharpReader/MainWindow.cs
@@ -15,6 +15,28 @@ namespace LASSharpReader
         public MainWindow()
         {
             InitializeComponent();
+
+            string text;
+            if (string.Compare(fileLabel.Text, "Loaded file: ") <= 0)
+            {
+                text = "No file loaded";
+            }
+            else
+            {
+                text = fileLabel.Text;
+            }
+            loadedFileLabelTooltip.SetToolTip(fileLabel, text);
+        }
+
+        private void loadLASButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openLASDialog.ShowDialog( this );
+
+            if (result == DialogResult.OK)
+            {
+                fileLabel.Text = "File loaded: " + openLASDialog.FileName;
+                loadedFileLabelTooltip.SetToolTip(fileLabel, openLASDialog.FileName);
+            }
         }
     }
 }
