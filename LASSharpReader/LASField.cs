e67df6599c2f771a261bd83e19612d66287814ec
Bruno <killerasus@gmail.com>
2016-01-15 23:49:58 -0200

    Adds the LASField struct for holding a line of a information section

---
+++ LASSharpReader/LASField.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LASSharpReader
+{
+    /// <summary>
+    /// Represents a LAS line field
+    /// </summary>
+    public struct LASField
+    {
+        
+        private string _mnemonic;
+        private string _value;
+        private string _comment;
+
+        /// <summary>
+        /// Mnemonic column
+        /// </summary>
+        public string Mnemonic
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Value column
+        /// </summary>
+        public string Value
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        /// <summary>
+        /// Comment column
+        /// </summary>
+        public string Comment
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+    }
+}
