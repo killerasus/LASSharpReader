cc96324f22d55d68a879d3df6d7d934b3efc73cb
Bruno <killerasus@gmail.com>
2016-01-15 22:04:11 -0200

    Added LASFileReader for reading LAS 2.0 file. Added LASSharpAbout for showing About dialog.
    Created Resources for en-US and pt-BR languages.

---
+++ LASSharpReader/LASFileReader.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LASSharpReader
+{
+    /// <summary>
+    /// Implements a LAS 2.0 file reader
+    /// </summary>
+    class LASFileReader
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public LASFileReader( )
+        {
+
+        }
+        
+        public bool ReadFile( string LASFilePath, ref string errorMessage  )
+        {
+            bool valid = false;
+            _filePath = LASFilePath;
+            
+            try
+            {
+                System.IO.FileStream lasFile;
+                lasFile = System.IO.File.OpenRead(_filePath);
+
+                if (lasFile.Length > 0)
+                {
+                    ///TODO: Implement LAS file reading treatment
+                    valid = true;
+                }
+                else
+                {
+                    errorMessage = "File size = 0.";
+                    valid = false;
+                }
+
+                lasFile.Close();
+            }
+            catch(ArgumentNullException exception)
+            {
+                errorMessage = exception.Message;
+                valid = false;
+            }
+            catch(System.IO.PathTooLongException exception)
+            {
+                errorMessage = exception.Message;
+                valid = false;
+            }
+            catch(System.IO.DirectoryNotFoundException exception)
+            {
+                errorMessage = exception.Message;
+                valid = false;
+            }
+            catch(UnauthorizedAccessException exception)
+            {
+                errorMessage = exception.Message;
+                valid = false;
+            }
+            catch(System.IO.FileNotFoundException exception)
+            {
+                errorMessage = exception.Message;
+                valid = false;
+            }
+            
+            return valid;
+        }
+
+        /// <summary>
+        /// Returns the filepath associated in readFile
+        /// </summary>
+        public string FilePath
+        {
+            get
+            {
+                return _filePath;
+            }
+        }
+
+        /// <summary>
+        /// Path to the current open LAS file
+        /// </summary>
+        private string _filePath;
+    }
+}
--- LASSharpReader/Properties/Resources.Designer.cs
+++ LASSharpReader/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LASSharpReader.Properties
-{
-
-
+namespace LASSharpReader.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,104 @@ namespace LASSharpReader.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("LASSharpReader.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &amp;About.
+        /// </summary>
+        internal static string about {
+            get {
+                return ResourceManager.GetString("about", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &amp;Close.
+        /// </summary>
+        internal static string close {
+            get {
+                return ResourceManager.GetString("close", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &amp;File.
+        /// </summary>
+        internal static string file {
+            get {
+                return ResourceManager.GetString("file", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &amp;Help.
+        /// </summary>
+        internal static string help {
+            get {
+                return ResourceManager.GetString("help", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Loaded file: .
+        /// </summary>
+        internal static string loadedFile {
+            get {
+                return ResourceManager.GetString("loadedFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to No file loaded..
+        /// </summary>
+        internal static string noFileLoaded {
+            get {
+                return ResourceManager.GetString("noFileLoaded", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to &amp;Open.
+        /// </summary>
+        internal static string open {
+            get {
+                return ResourceManager.GetString("open", resourceCulture);
             }
         }
     }
